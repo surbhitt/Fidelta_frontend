55bf884f82e9f70d8830ac128e9a4b54b25e1c54
surbhitt <surbhit.thakur@gmail.com>
2023-11-01 12:56:20 +0530

    third

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -44,7 +44,67 @@ export const Service = () => {
 export const Reports = () => {
   return (
     <div className="report">
-      <h1>FIDELTA Reports</h1>
+      <div className="w-full">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xl text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                TokenId
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Pseudo
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Acoount
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-lg">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                ETC13dfs45b32j455ajsdf29834asda23fad7
+              </th>
+              <td className="px-6 py-4">pseudo13dfs</td>
+              <td className="px-6 py-4">Type1Loyalty +3</td>
+              <td className="px-6 py-4">
+                W8fdDTZP85V.*`k?|:LAFj%r9eay@l8OVQFZZ%V?t9
+              </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                ETC13dgsSG723934anG234mxsf2343fad8
+              </th>
+              <td className="px-6 py-4">pseudo13dgs</td>
+              <td className="px-6 py-4">Type1Loyalty +2</td>
+              <td className="px-6 py-4">
+                q+)2Te~|~,s;J;=kGoi1up^@k=Hj&_/+&&./ragr!n`3Ag!QOG+
+              </td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                ETC13d2gs45asdfHHSdfa34gG14arsddf35
+              </th>
+              <td className="px-6 py-4">pseudo13d2g</td>
+              <td className="px-6 py-4">Type1Loyalty +1</td>
+              <td className="px-6 py-4">
+                q+)2Te~|~,s;J;=kGoi1up^@k=Hj&_/+&&./ragr!n`3Ag!QOG+
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
