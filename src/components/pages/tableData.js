f34dc84907c28de6204cdfd0eeb82456c78a475c
surbhitt <surbhit.thakur@gmail.com>
2023-12-17 14:03:27 +0530

    feat: corrected table

--- src/components/pages/tableData.js
+++ src/components/pages/tableData.js
@@ -1,12 +1,12 @@
 
 const data  = [
-	{ethuuid: '#5', pseudo: 'shit', chunkId: 1, deposit: '2000'},
-	{ethuuid: '#1', pseudo: 'shit', chunkId: 1, deposit: '2000'},
-	{ethuuid: '#2', pseudo: 'shit', chunkId: 1, deposit: '2000'},
-	{ethuuid: '#3', pseudo: 'shit', chunkId: 1, deposit: '2000'},
-	{ethuuid: '#4', pseudo: 'shit', chunkId: 1, deposit: '2000'},
-	{ethuuid: '#5', pseudo: 'shit', chunkId: 1, deposit: '2000'},
-	{ethuuid: '#6', pseudo: 'shit', chunkId: 1, deposit: '2000'},
-	{ethuuid: '#7', pseudo: 'shit', chunkId: 1, deposit: '2000'},
+	{ethuuid: 'ethcNzKv9ncq6MbUFM5aa', pseudo: '**NzKv9ncq6MbUFM5', chunkId: 89, deposit: '20'},
+	{ethuuid: 'ethcHSZimnxcO6ElNdBaa', pseudo: '**HSZimnxcO6ElNdB', chunkId:233, deposit: '20'},
+	{ethuuid: 'ethcBHo4NPxr8FrcsTFaa', pseudo: '**BHo4NPxr8FrcsTF', chunkId: 322, deposit: '2000'},
+	{ethuuid: 'ethcPxwadLUZy6n1S6Caa', pseudo: '**PxwadLUZy6n1S6C', chunkId:98, deposit: '8000'},
+	{ethuuid: 'ethcXIe8GEW40DXiXkWaa', pseudo: '**XIe8GEW40DXiXkW', chunkId: 297, deposit: '2000'},
+	{ethuuid: 'ethc', pseudo: '**', chunkId: 890, deposit: '2000'},
+	{ethuuid: 'ethcLHo4NPxr8FrcsTFaa', pseudo: '**LHo4NPxr8FrcsTF', chunkId: 983, deposit: '28'},
+	{ethuuid: 'ethc3HCq3UW6dfnQVzZaa', pseudo: '**3HCq3UW6dfnQVzZ', chunkId: 878, deposit: '200'},
 ]
 export default data
